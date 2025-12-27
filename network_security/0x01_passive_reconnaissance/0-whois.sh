aff2d3b1fa2abd7bf3c9c3bf83e1ce846a67489d
ravan33-120 <ravannabiyev466@gmail.com>
2025-12-27 16:03:35 +0000

    whois

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F': ' '/^Registrant |^Admin |^Tech / {gsub(/^[ \t]+|[ \t]+$/, "", $2); if ($1 ~ /Street/) $2=$2" "; print $1 "," $2}' > "$1.csv"
+whois "$1" | awk -F: '/^Registrant|^Admin|^Tech/ {if ($1 ~ /Ext/) $1=$1":"; if ($2=="") $2=" "; print $1 "," $2}' > "${1}.csv"
